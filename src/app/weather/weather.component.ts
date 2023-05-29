9f83a365994d7a933ae7f41738f5a1d86f56534c
Tim Burmester <Tim-Burmester1@web.de>
2023-05-29 17:00:43 +0200

    Displaying some data

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -11,6 +11,7 @@ import { ApixuService } from '../apixu.service';
 
 export class WeatherComponent implements OnInit {
   public weatherSearchForm: FormGroup;
+  public weatherData: any;
   constructor(private formBuilder: FormBuilder,
     private apixuService: ApixuService
     ) { }
@@ -26,7 +27,10 @@ export class WeatherComponent implements OnInit {
  
     this.apixuService
     .getWeather(formValues.location)
-    .subscribe(data => console.log(data));
+    .subscribe(data =>{
+      this.weatherData = data;
+      console.log(this.weatherData)
+    });
 }
 
 }
